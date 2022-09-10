d7f0838f9f8e3e2da452d3640c9b4d3762dbdf41
tianyu <tianyu.li02@icloud.com>
2022-09-10 02:43:58 -0400

    1, 2, 3, 4 completed

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -196,6 +196,7 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        return 1;
     }
 
     /*
